2a70c9763cd34c58e9f3bb047d61c1fdd2295afb
zhengsai <gavinzhs@gmail.com>
2015-07-27 16:36:30 +0800

    add basic handler and server

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
+import (
+    "net/http")
 
 func main(){
     print("start")
+
+    http.HandleFunc("/", mainHandler)
+    http.HandleFunc("/test1", test1Handler)
+    http.HandleFunc("/test2", test2Handler)
+
+    http.ListenAndServe(":3000", nil)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request){
+    w.Write([]byte("main"))
+}
+
+func test1Handler(w http.ResponseWriter, r *http.Request){
+    w.Write([]byte("test1"))
+}
+
+func test2Handler(w http.ResponseWriter, r *http.Request){
+    w.Write([]byte("test2"))
 }
