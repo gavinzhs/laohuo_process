530a0619eb9139b8b604cb05e99fc0bac0be0b1c
zhengsai <gavinzhs@gmail.com>
2015-08-20 12:00:54 +0800

    modify port

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	log.Println("start")
 
-	port := flag.String("p", ":3000", "address the server listen on")
+	port := flag.String("p", ":4000", "address the server listen on")
 	war := flag.String("war", "./public", "directory of war files")
 	flag.Parse()
 
